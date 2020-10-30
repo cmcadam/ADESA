01c1ac8f02bda83ea5aefdfb4caac6f736095a78
Christopher McAdam <cmca9796@uni.sydney.edu.au>
2020-10-30 14:50:06 +1100

    updated email configurations

--- ADESA/settings.py
+++ ADESA/settings.py
@@ -154,5 +154,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'chrismcadam21@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_USER = 'YOUR EMAIL'
+EMAIL_HOST_PASSWORD = 'YOUR PASSWORD'
